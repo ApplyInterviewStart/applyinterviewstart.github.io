991a089d5a2c65274c8a756ba13e3ae594bdab20
ApplyInterviewStart <applyinterviewstart@outlook.com>
2026-01-05 12:09:22 -0800

    Initial site upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function buyResume() {
+  alert("Stripe checkout will be connected soon!");
+}
+
+function buyInterview() {
+  alert("Stripe checkout will be connected soon!");
+}
+
+function buyBundle() {
+  alert("Stripe checkout will be connected soon!");
+}
